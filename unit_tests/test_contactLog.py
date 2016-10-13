4e9a6c3d877924d0fa2afcf1358655753933a4dc
auser5707@saskpolytech.ca <auser5707@saskpolytech.ca>
2016-10-13 14:32:16 -0600

    Committing unit test file, which now has algorithms for unit testing.

--- unit_tests/test_contactLog.py
+++ unit_tests/test_contactLog.py
@@ -4,6 +4,47 @@ from contact_log import contact_log
 
 class TestContactLog(TestCase):
     def test_createContactLog(self):
+        #Unit Test Information
+
+        #Test: Test that a contact log is saved to the DB
+        #       if all entered information is valid
+
+        #Input:
+        #   Date: 10/05/2015
+        #   Description: "Hello"
+        #   Member ID: NULL
+
+        #Expected Result: Contact Log is saved to the DB
+
+        # Input:
+        #   Date: 10/35/2015
+        #   Description: "Hello"
+        #   Member ID: NULL
+
+        # Expected Result: Contact Log is NOT saved, error is displayed
+
+        # Input:
+        #   Date: 10/05/2015
+        #   Description: ""
+        #   Member ID: NULL
+
+        # Expected Result: Contact Log is saved to the DB
+
+        # Input:
+        #   Date: 10/05/2015
+        #   Description: "Hello"
+        #   Member ID: 1
+
+        #Assumption: A member with ID 1 is present in the Member table
+        # Expected Result: Contact Log is saved to the DB
+
+        # Input:
+        #   Date: 10/05/2015
+        #   Description: "Hello"
+        #   Member ID: 50
+
+        # Assumption: A member with ID 50 is NOT present in the Member table
+        # Expected Result: Contact Log is NOT saved to the DB, error is displayed
         self.fail()
 
     def test_validateDate(self):
@@ -13,6 +54,25 @@ class TestContactLog(TestCase):
         self.fail()
 
     def test_validateMemberID(self):
+        # Unit Test Information
+        # Test: Test that a memberID is exactly nine digits
+        # Input: 123456789
+        # Expected return: true
+
+        #Input: 000000000
+        #Expected return: true
+
+        #Input: 999999999
+        #Expected return: true
+
+        #Input: 0
+        #Expected return: false
+
+        #Input helloWorld
+        #Expected return: false
+
+        #Input "" (N/A)
+        #Expected return: false
         self.fail()
 
     def test_setContactLogID(self):
