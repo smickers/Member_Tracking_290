8e7b88ae773805368909fffc04cad8a1e7acf60f
auser5707@saskpolytech.ca <auser5707@saskpolytech.ca>
2016-10-13 14:17:37 -0600

    Committing initial files for unit testing.

---
+++ contact_log/contact_log.py
@@ -0,0 +1,18 @@
+class ContactLog:
+    def createContactLog(self, description, contactDate, memberID):
+        5 * 2
+    #
+
+    def validateDate(self, toCheck):
+        5 * 2
+
+    def validateDescription(self, toCheck):
+        5 * 2
+
+    def validateMemberID(self, toCheck):
+        5 * 2
+
+    def setContactLogID(self, newID):
+        5 * 2
+
+
---
+++ unit_tests/test_contactLog.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from contact_log import contact_log
+
+
+class TestContactLog(TestCase):
+    def test_createContactLog(self):
+        self.fail()
+
+    def test_validateDate(self):
+        self.fail()
+
+    def test_validateDescription(self):
+        self.fail()
+
+    def test_validateMemberID(self):
+        self.fail()
+
+    def test_setContactLogID(self):
+        self.fail()
