f19cc94765d8874e78fcd1340f65471ed5542563
Megan K <kostiuk9589@saskpolytech.ca>
2016-10-17 15:30:24 +0000

    Finished test cases for date and description

--- unit_tests/test_contactLog.py
+++ unit_tests/test_contactLog.py
@@ -48,6 +48,26 @@ class TestContactLog(TestCase):
         self.fail()
 
     def test_validateDate(self):
+
+        #Input: 01/01/2016
+        #Expected Results: Date will be correct
+
+        # Input: 12/12/2016
+        # Expected Results: Date will be correct
+
+        # Input: January 1, 2016
+        # Expected Results: Date will be correct
+
+        # Input: 30/02/2016
+        # Expected Results: Date will be incorrect because Feb has 29 days in 2016
+
+        # Input: 29/02/2015
+        # Expected Results: Date will be incorrect because Feb has 28 days in 2015
+
+        # Input: 01/30/2016
+        # Expected Results: Date will be incorrect because there are only up to 12 months
+
+
         self.fail()
 
     def test_validateDescription(self):
@@ -68,6 +88,21 @@ class TestContactLog(TestCase):
         ornare, diam in accumsan eleifend, tortor odio suscipit dui, nec auctor orci lacus ut urna. Cras ac purus nec dui.
 
         Expected Result: true
+
+
+        Input
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque commodo viverra lacinia. Proin id sollicitudin
+        urna. Cras a massa eleifend, interdum mi nec, tempus enim. Donec eget ligula nec leo fermentum imperdiet vel
+        et turpis. Donec dapibus nec enim ut rhoncus. Donec dictum est eleifend sollicitudin maximus. Morbi viverra
+        volutpat ullamcorper. Nulla interdum ornare elementum. Nullam orci leo, faucibus at semper id, malesuada ac mi.
+        Quisque facilisis sem arcu, in ultricies erat lobortis vitae. Vestibulum pellentesque tempus ex, ac fringilla
+        erat euismod sit amet. Etiam vel nisi euismod, malesuada nisi id, vulputate nisi. Quisque nec interdum tortor.
+
+        Suspendisse varius nisl metus, in rutrum tellus pharetra et. In nulla ligula, maximus quis sodales ac, semper
+        nec neque. Etiam porttitor tortor in luctus rutrum. In elementum magna sed augue consectetur iaculis. Donec
+        ornare, diam in accumsan eleifend, tortor odio suscipit dui, nec auctor orci lacus ut urna. Cras ac purus nec dui. Lorem
+
+        Expected Result: False
         '''
 
         self.fail()
