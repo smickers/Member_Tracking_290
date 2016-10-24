4b652f15ea6c913c64ab588a6c8af2afeeceeecb
auser5707@saskpolytech.ca <auser5707@saskpolytech.ca>
2016-10-24 09:51:57 -0600

    Started our story over again. Put together a model and some unit tests. -- Cam -- Megan

--- contact_log/contact_log_url.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from . import contact_log_view
-
-urlpatterns = [
-    url(r'^.*$',contact_log_view.index, name='index'),
-]
--- spfa_mt/settings.py
+++ spfa_mt/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'contact_log.apps.ContactLogConfig',
+    'contactLog.apps.ContactlogConfig'
 ]
 
 MIDDLEWARE = [
