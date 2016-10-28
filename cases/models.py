7c7f5334fed88a8221e4f9bee9963f8779d5dc45
dletkeman <letkeman0812@saskpolytech.ca>
2016-10-28 21:28:17 +0000

    Finished the code waiting to demo to ernesto

--- cases/models.py
+++ cases/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 import sys
 
 
@@ -16,11 +17,11 @@ class Member(models.Model):
             Case.members.append(memberID)
         else:
             ValueError("memberID must be 9 digits")
-        CaseMembers.members.save()
+        CaseMembers.memberNum.save()
 
-
+#Comment to hopefully detect changes
 class Case(models.Model):
-    caseID  = models.CharField(max_length=9,unique='true')
+    caseID = models.CharField(max_length=9,unique='true')
 
     def addCase(caseID):
         assert isinstance(caseID, Case)
@@ -28,9 +29,16 @@ class Case(models.Model):
             CaseMembers.caseNum = caseID
         else:
             ValueError("caseID must be 9 digits")
-        CaseMembers.caseNum.save
+
 
 class CaseMembers(models.Model):
-    caseNum = models.ForeignKey(Case, on_delete=models.CASCADE, to_field='caseID')
-    memberNum = models.ForeignKey(Member, on_delete=models.CASCADE, to_field='memberID')
+    caseNum = models.CharField(max_length=9)
+    memberNum = models.CharField(max_length=9)
+    caseMember = models.CharField(max_length=18, unique='true', null='true')
 
+    def save (self):
+        self.caseMember = self.caseNum + self.memberNum
+        super(CaseMembers, self).save()
+
+    def get_absolute_url(self):
+        return reverse(viewname='cases:addMemberToCase')
--- cases/urls.py
+++ cases/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'cases'
 
-urlpatterns = {
+urlpatterns = [
     url(r'^$', views.CaseMembersCreate.as_view(), name='addMemberToCase'),
-}
+]
