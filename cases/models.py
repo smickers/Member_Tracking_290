da756b99eb6ebb8425058f1972e6a9bc27ade604
dletkeman <letkeman0812@saskpolytech.ca>
2016-11-03 21:04:08 +0000

    Added error checking to the models --David & Steph

--- cases/models.py
+++ cases/models.py
@@ -1,27 +1,31 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.core.urlresolvers import reverse
+from add_member.models import Person
 import sys
 
 
 class Member(models.Model):
-    memberID = models.CharField(max_length=9,unique='true')
+    memberID = models.CharField(max_length=9, unique='true')
     firstName = models.CharField(max_length=20)
     lastName = models.CharField(max_length=30)
 
     def __str__(self):
         return self.firstName + " " + self.lastName
 
-    def  addmember(memberID):
+    def addmember(memberID):
         assert isinstance(memberID, Member)
         if sys.getsizeof(memberID) == 9:
             Case.members.append(memberID)
         else:
             ValueError("memberID must be 9 digits")
-        CaseMembers.memberNum.save()
+        CaseMembers.memberNum.do_save()
+
 
 #Comment to hopefully detect changes
+# noinspection PyMethodParameters
 class Case(models.Model):
-    caseID = models.CharField(max_length=9,unique='true')
+    caseID = models.CharField(max_length=9, unique='true')
 
     def addCase(caseID):
         assert isinstance(caseID, Case)
@@ -31,14 +35,31 @@ class Case(models.Model):
             ValueError("caseID must be 9 digits")
 
 
+# noinspection PyGlobalUndefined
 class CaseMembers(models.Model):
     caseNum = models.CharField(max_length=9)
     memberNum = models.CharField(max_length=9)
-    caseMember = models.CharField(max_length=18, unique='true', null='true')
+    caseMember = models.CharField(max_length=18, unique='true', null='true', )
 
-    def save (self):
-        self.caseMember = self.caseNum + self.memberNum
-        super(CaseMembers, self).save()
+    # noinspection PyGlobalUndefined
+    def save(self):
+        caseCheck = False
+        memberCheck = False
+        try:
+            Case.objects.get(caseID=self.caseNum)
+            caseCheck = True
+        except ObjectDoesNotExist:
+            print ("Case Does not exist")
+        try:
+            Person.objects.get(memberID=self.memberNum)
+            memberCheck = True
+        except Exception:
+            print ("Member Does not exist")
+        if(memberCheck & caseCheck):
+            self.caseMember = self.caseNum + self.memberNum
+            super(CaseMembers, self).save()
+            print("Member added to Case")
 
-    def get_absolute_url(self):
+    @staticmethod
+    def get_absolute_url():
         return reverse(viewname='cases:addMemberToCase')
