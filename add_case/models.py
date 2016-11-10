5760e6146cc57594369a8ab88f1fdd24d1652ee5
dletkeman <letkeman0812@saskpolytech.ca>
2016-11-10 19:16:44 +0000

    commit to resolve errors. --David L Steph M

--- add_case/models.py
+++ add_case/models.py
@@ -4,16 +4,21 @@ from django.db import models
 from django.core.validators import MaxValueValidator
 
 
-
 class Case(models.Model):
     lead = models.IntegerField(max_length=9)
     complainant = models.IntegerField(max_length=9)
     campus = models.CharField(max_length=20)
     school = models.CharField(max_length=255)
     caseType = models.CharField(max_length=50, validators=[validate_case_type])
-    status = models.CharField(max_length=50, blank=True, validators=[validate_status])
-    additionalMembers = models.IntegerField(max_length=9, blank=True, null=True)
-    additionalNonMembers = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=50, validators=[validate_status])
+    additionalMembers = models.TextField(blank=True)
+    additionalNonMembers = models.TextField(blank=True)
     docs = models.TextField(blank=True, null=True)
     logs = models.TextField(blank=True, null=True)
     date = models.DateField(blank=True, null=True, validators=[validate_date])
+
+
+class CaseMembers(models.Model):
+    caseNum = models.CharField(max_length=9)
+    memberNum = models.TextField()
+    caseMember = models.CharField(max_length=18, unique='true', null='true')
