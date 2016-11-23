5a75b19b114d3c0e294b6c8f82051086e9640169
Daven Paul Casia <casia3115@saskpolytech.ca>
2016-11-23 11:34:17 -0500

    added some stuff

--- spfa_mt/urls.py
+++ spfa_mt/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^addmember/', include('add_member.urls')),
     url(r'^cases/', include('cases.urls')),
-    url(r'^addCase/', include('add_case.urls'))
+    url(r'^addCase/', include('add_case.urls')),
     url(r'^contact_log/', include('contactLog.urls')),
     url(r'^member/', include('add_member.urls')),
 ]
