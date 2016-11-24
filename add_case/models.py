0b66f6ef05afa0efa82041d753e692fa13ed8d81
Megan Kostiuk <kostiuk9589@saskpolytech.ca>
2016-11-24 14:05:42 -0500

    --made some changes so select boxes prefill with data -megan -jaryd

--- add_case/models.py
+++ add_case/models.py
@@ -1,4 +1,7 @@
 from __future__ import unicode_literals
+
+from django.db.transaction import on_commit
+
 from .validators import *
 from django.db import models
 from django.core.validators import MaxValueValidator
@@ -6,15 +9,68 @@ from django.core.urlresolvers import reverse
 import datetime
 
 
+
 # Creating the model for the case
 class Case(models.Model):
+    STATUS = [
+        ("OPEN","OPEN"),
+        ("CLOSED","CLOSED"),
+        ("PENDING","PENDING"),
+        ("ACTION REQ'D - MGMT","ACTION REQ'D - MGMT"),
+        ("ACTION REQ'D SPFA","ACTION REQ'D SPFA"),
+    ]
+
+    CASE_TYPE = [
+        ("GRIEVANCES - INDIVIDUAL","GRIEVANCES - INDIVIDUAL"),
+        ("GRIEVANCES - GROUP","GRIEVANCES - GROUP"),
+        ("GRIEVANCES - POLICY","GRIEVANCES - POLICY"),
+        ("GRIEVANCES - CLASSIFICATION","GRIEVANCES - CLASSIFICATION"),
+        ("GRIEVANCES - COMPLAINTS","GRIEVANCES - COMPLAINTS"),
+        ("DISABILITY CLAIMS","DISABILITY CLAIMS"),
+        ("ARBITRATION","ARBITRATION"),
+        ("COMPLAINT","COMPLAINT"),
+    ]
+    CAMPUS_CHOICE = [
+        ('Saskatoon', 'Saskatoon'),
+        ('Regina', 'Regina'),
+        ('Moose Jaw', 'Moose Jaw'),
+        ('Prince Albert', 'Prince Albert'),
+    ]
+    SCHOOLS=[
+        ("Business","Business"),
+        ("Construction", "Construction"),
+        ("Health Sciences", "Health Sciences"),
+        ("Hospitality and Tourism", "Hospitality and Tourism"),
+        ("Human Services and Community Safety", "Human Services and Community Safety"),
+        ("Information and Communications Technology", "Information and Communications Technology"),
+        ("Mining, Energy and Manufacturing", "Mining, Energy and Manufacturing"),
+        ("Natural Resources and Built Environment", "Natural Resources and Built Environment"),
+        ("Nursing", "Nursing"),
+        ("Transportation", "Transportation"),
+    ]
+
+
+    DEPARTMENTS=[
+        ("Fitness Centre", "Fitness Centre"),
+        ("ILDC", "ILDC"),
+        ("Learning Services", "Learning Services"),
+        ("Learning Technologies","Learning Technologies"),
+        ("Library","Library"),
+        ("PLAR","PLAR"),
+        ("Simulation Lab","Simulation Lab"),
+        ("Student Development","Student Development"),
+    ]
+
     lead = models.IntegerField(max_length=9)
-    complainant = models.IntegerField(max_length=9)
-    campus = models.CharField(max_length=20)
-    school = models.CharField(max_length=255)
-    department = models.CharField(max_length=255, null=True)
-    caseType = models.CharField(max_length=50, validators=[validate_case_type])
-    status = models.CharField(max_length=50, blank=True, default='OPEN', validators=[validate_status])
+    #complainant = models.IntegerField(max_length=9)
+    #IMPORTANT: NEED TO CHANGE TEST CASES BEFORE DOING THIS
+    #note for future references: the select box will show what the parent's to string method returns
+    complainant = models.ForeignKey('add_member.Person', on_delete=models.CASCADE, blank=True)
+    campus = models.CharField(choices=CAMPUS_CHOICE, default='Saskatoon', max_length=20)
+    school = models.CharField(choices=SCHOOLS, default='Business', max_length=255)
+    department = models.CharField(choices=DEPARTMENTS,default='Learning Technologies', max_length=255, null=True)
+    caseType = models.CharField(choices=CASE_TYPE, max_length=50, default='GRIEVANCES - INDIVIDUAL', validators=[validate_case_type])
+    status = models.CharField(choices=STATUS,  default='OPEN', max_length=50, blank=True,validators=[validate_status])
     additionalMembers = models.IntegerField(max_length=9, blank=True, null=True)
     additionalNonMembers = models.TextField(blank=True, null=True)
     docs = models.TextField(blank=True, null=True)
