6e8ab07e181d5c497882e6960b3caec10df0a9e6
Megan Kostiuk <kostiuk9589@saskpolytech.ca>
2016-11-30 09:50:01 -0500

    --cleaned up unit tests -megan -jaryd

--- add_case/models.py
+++ add_case/models.py
@@ -12,63 +12,13 @@ import datetime
 
 # Creating the model for the case
 class Case(models.Model):
-    STATUS = [
-        ("OPEN","OPEN"),
-        ("CLOSED","CLOSED"),
-        ("PENDING","PENDING"),
-        ("ACTION REQ'D - MGMT","ACTION REQ'D - MGMT"),
-        ("ACTION REQ'D SPFA","ACTION REQ'D SPFA"),
-    ]
-
-    CASE_TYPE = [
-        ("GRIEVANCES - INDIVIDUAL","GRIEVANCES - INDIVIDUAL"),
-        ("GRIEVANCES - GROUP","GRIEVANCES - GROUP"),
-        ("GRIEVANCES - POLICY","GRIEVANCES - POLICY"),
-        ("GRIEVANCES - CLASSIFICATION","GRIEVANCES - CLASSIFICATION"),
-        ("GRIEVANCES - COMPLAINTS","GRIEVANCES - COMPLAINTS"),
-        ("DISABILITY CLAIMS","DISABILITY CLAIMS"),
-        ("ARBITRATION","ARBITRATION"),
-        ("COMPLAINT","COMPLAINT"),
-    ]
-    CAMPUS_CHOICE = [
-        ('Saskatoon', 'Saskatoon'),
-        ('Regina', 'Regina'),
-        ('Moose Jaw', 'Moose Jaw'),
-        ('Prince Albert', 'Prince Albert'),
-    ]
-    SCHOOLS=[
-        ("School of Business", "Business"),
-        ("School of Construction", "Construction"),
-        ("School of Health Sciences", "Health Sciences"),
-        ("School of Hospitality and Tourism", "Hospitality and Tourism"),
-        ("School of Human Services and Community Safety", "Human Services and Community Safety"),
-        ("School of Information and Communications Technology", "Information and Communications Technology"),
-        ("School of Mining, Energy and Manufacturing", "Mining, Energy and Manufacturing"),
-        ("School of Natural Resources and Built Environment", "Natural Resources and Built Environment"),
-        ("School of Nursing", "Nursing"),
-        ("School of Transportation", "Transportation"),
-    ]
-
-
-    #for another story - have both departments and non school departments as valid
-    """NON_SCHOOL_DEPARTMENTS=[
-        ("Fitness Centre", "Fitness Centre"),
-        ("ILDC", "ILDC"),
-        ("Learning Services", "Learning Services"),
-        ("Learning Technologies","Learning Technologies"),
-        ("Library","Library"),
-        ("PLAR","PLAR"),
-        ("Simulation Lab","Simulation Lab"),
-        ("Student Development","Student Development"),
-    ]"""
-
     lead = models.IntegerField(max_length=9)
     complainant = models.IntegerField(max_length=9)
-    campus = models.CharField(choices=CAMPUS_CHOICE, default='Saskatoon', max_length=20)
-    school = models.CharField(choices=SCHOOLS, default='Business', max_length=255)
+    campus = models.CharField(max_length=20)
+    school = models.CharField(max_length=255)
     department = models.CharField(max_length=255, null=True)
-    caseType = models.CharField(choices=CASE_TYPE, max_length=50, default='GRIEVANCES - INDIVIDUAL', validators=[validate_case_type])
-    status = models.CharField(choices=STATUS,  default='OPEN', max_length=50, blank=True,validators=[validate_status])
+    caseType = models.CharField(max_length=50, default='GRIEVANCES - INDIVIDUAL', validators=[validate_case_type])
+    status = models.CharField(default='OPEN', max_length=50, blank=True,validators=[validate_status])
     additionalMembers = models.IntegerField(max_length=9, blank=True, null=True)
     additionalNonMembers = models.TextField(blank=True, null=True)
     docs = models.TextField(blank=True, null=True)
