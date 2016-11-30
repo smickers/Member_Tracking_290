d772d5c9a2fd96e36534286fed4644fa4eb18c89
bitnami <casia3115@saskpolytech.ca>
2016-11-30 14:17:25 +0000

    fixed URL config

--- spfa_mt/urls.py
+++ spfa_mt/urls.py
@@ -4,8 +4,9 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^contact_log/',include('contactLog.urls')),
-    url(r'^addCase/', include('add_case.urls')),
     url(r'^addmember/', include('add_member.urls')),
-    url(r'^add_event/', include('create_event.urls'))
+    url(r'^cases/', include('cases.urls')),
+    url(r'^addCase/', include('add_case.urls')),
+    url(r'^contact_log/', include('contactLog.urls')),
+    url(r'^member/', include('add_member.urls')),
 ]
