aa9082454b6eea9d54bf6ee1c0ca3a2bff9f50f0
Daven Paul Casia <casia3115@saskpolytech.ca>
2016-11-30 23:24:56 -0600

    added some failing tests in preparation for pre-code checklist demo

--- create_event/tests.py
+++ create_event/tests.py
@@ -1,21 +1,83 @@
 from django.test import TestCase
+import unittest
 from django.db import IntegrityError
 from django.core.exceptions import ValidationError
-from models import Event
+from create_event.models import Event
 from django.db import DataError
 import datetime
+from add_member.models import Person
 
 
-# region Description: Test For Event Creation
-class EventTest(TestCase):
 
+class EventTest(unittest.TestCase):
+    testEvent = None
+    tempPerson = None
+    tempPerson2 = None
+
+    def setUp(self):
+        self.testEvent = Event()
+        self.testEvent.name = "Sample Name"
+        self.testEvent.description = ""
+        self.testEvent.date = "2016-12-25"
+        self.testEvent.location = "Saskatoon"
+        self.testEvent.full_clean()
+        self.testEvent.save()
+
+        self.tempPerson = Person()
+        self.tempPerson.memberID = 123456789
+        self.tempPerson.firstName = 'First'
+        self.tempPerson.middleName = 'Middle'
+        self.tempPerson.lastName = 'Last'
+        self.tempPerson.socNum = 123456789
+        self.tempPerson.city = 'Sample City'
+        self.tempPerson.mailAddress = 'Sample address'
+        self.tempPerson.mailAddress2 = 'Sample Address 2'
+        self.tempPerson.pCode = 's7k5j8'
+        self.tempPerson.hPhone = '(306)812-1234'
+        self.tempPerson.cPhone = '(306)812-1234'
+        self.tempPerson.hEmail = 'sample@sample.com'
+        self.tempPerson.campus = 'SASKATOON'
+        self.tempPerson.jobType = 'FTO'
+        self.tempPerson.committee = 'Sample Commitee'
+        self.tempPerson.memberImage = 'image.img'
+        self.tempPerson.bDay = '2012-03-03'
+        self.tempPerson.hireDate = '2012-03-03'
+        self.tempPerson.gender = 'MALE'
+        self.tempPerson.membershipStatus = 'RESOURCE'
+        self.tempPerson.programChoice = 'Sample Program'
+        self.tempPerson.full_clean()
+        self.tempPerson.save()
+
+        self.tempPerson2 = Person()
+        self.tempPerson2.memberID = 123456789
+        self.tempPerson2.firstName = 'First'
+        self.tempPerson2.middleName = 'Middle'
+        self.tempPerson2.lastName = 'Last'
+        self.tempPerson2.socNum = 123456789
+        self.tempPerson2.city = 'Sample City'
+        self.tempPerson2.mailAddress = 'Sample address'
+        self.tempPerson2.mailAddress2 = 'Sample Address 2'
+        self.tempPerson2.pCode = 's7k5j8'
+        self.tempPerson2.hPhone = '(306)812-1234'
+        self.tempPerson2.cPhone = '(306)812-1234'
+        self.tempPerson2.hEmail = 'sample@sample.com'
+        self.tempPerson2.campus = 'SASKATOON'
+        self.tempPerson2.jobType = 'FTO'
+        self.tempPerson2.committee = 'Sample Commitee'
+        self.tempPerson2.memberImage = 'image.img'
+        self.tempPerson2.bDay = '2012-03-03'
+        self.tempPerson2.hireDate = '2012-03-03'
+        self.tempPerson2.gender = 'MALE'
+        self.tempPerson2.membershipStatus = 'RESOURCE'
+        self.tempPerson2.programChoice = 'Sample Program'
+        self.tempPerson2.full_clean()
+        self.tempPerson2.save()
     """
     Test 1 - Validate event name, empty name
     Input: ""
     Expected result: error thrown explaining event name is required
 
     """
-
     def test_event_name_empty_error(self):
         with self.assertRaises(ValidationError):
             testEvent = Event()
@@ -26,9 +88,9 @@ class EventTest(TestCase):
             testEvent.full_clean()
             testEvent.save()
 
-            # Test 2 - Validate event name, valid name
-            # Input: "A"
-            # Expected result: Event created and added to DB
+    # Test 2 - Validate event name, valid name
+    # Input: "A"
+    # Expected result: Event created and added to DB
 
     def test_event_name_valid(self):
         testEvent = Event()
@@ -194,11 +256,23 @@ class EventTest(TestCase):
             testEvent.full_clean()
             testEvent.save()
 
-# endregion
+    # end tests
+
+    # test for adding member to an event
+    def test_if_user_can_add_member_to_an_event(self):
+        self.testEvent.members.add(self.tempPerson)
+        self.assertEqual(self.members.count == 1)
 
 
-# region Description: Test For Adding event to an Event
-class EventTest(TestCase):
-    def test_here(self):
-        self.assertTrue(True)
-# endregion
+    def test_if_user_can_add_multiple_member_to_an_event(self):
+        self.testEvent.members.add(self.tempPerson)
+        self.testEvent.members.add(self.tempPerson2)
+        self.assertEqual(self.members.count == 2)
+
+    def test_if_user_cannot_add_a_member_that_already_exist_on_an_event(self):
+        self.testEvent.members.add(self.tempPerson)
+        self.testEvent.members.add(self.tempPerson)
+        self.assertEqual(self.members.count == 1)
+
+
+    # test
