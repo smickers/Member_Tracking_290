f54178131347c401db4aacef67977aa2b2965622
Cam <auser5707@saskpolytech.ca>
2016-12-01 15:49:10 -0500

    Beginning to put together a model definition.
    -- Cam
    -- David

--- grievance_award_creation/models.py
+++ grievance_award_creation/models.py
@@ -3,3 +3,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class GrievanceAward:
+    GRIEVANCE_TYPES = [
+        ('M', 'Member'),
+        ('P', 'Policy'),
+    ]
+
+    grievanceType = models.CharField(choices=GRIEVANCE_TYPES)
+    recipient = models.ManyToManyField()
+    case = models.ManyToManyField(null=True, blank=True)
+    awardAmount = models.DecimalField(min_value=0.01, max_value=999999.99)
+    description = models.CharField(max_length=1000, null=True,blank=True)
+    date = models.DateField()
