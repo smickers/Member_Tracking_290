0303d622d4d04095d9b3cfad687d90c9913d073e
Cam <auser5707@saskpolytech.ca>
2016-12-01 08:52:56 -0500

    Wrote some unit tests for this story. About half done writing unit tests now.
    -- Cam

--- grievance_award_creation/tests.py
+++ grievance_award_creation/tests.py
@@ -2,181 +2,167 @@ from django.test import TestCase
 from django.db import IntegrityError
 from django.core.exceptions import ValidationError
 from models import GrievanceAward
+from add_member.models import Person
+from add_case.models import Case
 from django.db import DataError
 import datetime
 
 class EventTest(TestCase):
-# Test 1 - Validate event name, empty name
-# Input: ""
-# Expected result: error thrown explaining event name is required
-    def test_event_name_empty_error(self):
+    tempPerson = Person()
+    person_pk = -1
+    tempCase = Case()
+    case_pk = -1
+    def setUp(self):
+        self.tempPerson.memberID = 1
+        self.tempPerson.firstName = 'First'
+        self.tempPerson.middleName = 'Middle'
+        self.tempPerson.lastName = 'Last'
+        self.tempPerson.socNum = 123456789
+        self.tempPerson.city = 'Sample City'
+        self.tempPerson.mailAddress = 'Sample address'
+        self.tempPerson.mailAddress2 = 'Sample Address 2'
+        self.tempPerson.pCode = 's7k5j8'
+        self.tempPerson.hPhone = '(306)812-1234'
+        self.tempPerson.cPhone = '(306)812-1234'
+        self.tempPerson.hEmail = 'sample@sample.com'
+        self.tempPerson.campus = 'SASKATOON'
+        self.tempPerson.jobType = 'FTO'
+        self.tempPerson.committee = 'Sample Commitee'
+        self.tempPerson.memberImage = 'image.img'
+        self.tempPerson.bDay = '2012-03-03'
+        self.tempPerson.hireDate = '2012-03-03'
+        self.tempPerson.gender = 'MALE'
+        self.tempPerson.membershipStatus = 'RESOURCE'
+        self.tempPerson.programChoice = 'Sample Program'
+        self.tempPerson.full_clean()
+        self.tempPerson.save()
+
+        self.person_pk = Person.objects.get(memberID=1).pk
+
+        self.tempCase = Case()
+        self.tempCase.lead = 1
+        self.tempCase.complainant = 1
+        self.tempCase.campus = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. " \
+                          "Aenean commodo ligula eget dolor. Aenean massa. Cum sociis " \
+                          "natoque penatibus et magnis dis parturient montes, nascetur " \
+                          "ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,"
+        self.tempCase.school = "School of Business"
+        self.tempCase.department = "Business Certificate"
+        self.tempCase.caseType = "GRIEVANCES - CLASSIFICATION"
+        self.tempCase.status = "OPEN"
+        self.tempCase.additionalMembers = 0
+        self.tempCase.additionalNonMembers = ""
+        self.tempCase.docs = None
+        self.tempCase.logs = None
+        self.tempCase.date = "2016-10-20"
+        self.tempCase.full_clean()
+        self.tempCase.save()
+
+        self.case_pk = Case.objects.get(lead=1).pk
+
+# Test 1 - Validate that a valid recipient can be added to a GrievanceAward
+# Input: Recipient is a valid PK
+# Expected result: Record is saved successfully
+    def test_valid_recipient(self):
+        ga = GrievanceAward
+        ga.recipient = self.person_pk
+        ga.awardAmount = 500.00
+        ga.description = ""
+        ga.date = '2016-12-01'
+        ga.full_clean()
+        ga.save()
+        self.assertTrue(True)
+
+# Test 2 - Validate that an invalid recipient throws an error
+# Input: Recipient is an invalid PK
+# Expected result: Error is thrown, record is not saved to DB.
+    def test_invalid_recipient(self):
         with self.assertRaises(ValidationError):
-            testEvent = Event()
-            testEvent.name = ""
-            testEvent.description = ""
-            testEvent.date = "2016-12-25"
-            testEvent.location = "Saskatoon"
-            testEvent.full_clean()
-            testEvent.save()
+            ga = GrievanceAward
+            ga.recipient = -3000
+            ga.awardAmount = 500.00
+            ga.description = ""
+            ga.date = '2016-12-01'
+            ga.full_clean()
+            ga.save()
 
-# Test 2 - Validate event name, valid name
-# Input: "A"
-# Expected result: Event created and added to DB
-    def test_event_name_valid(self):
-        testEvent = Event()
-        testEvent.name = "A"
-        testEvent.description = ""
-        testEvent.date = "2016-12-25"
-        testEvent.location = "Saskatoon"
-        testEvent.full_clean()
-        testEvent.save()
+# Test 3 - Validate that a valid case can be linked to a GrievanceAward
+# Input: case is a valid case PK
+# Expected result: Record is saved successfully
+    def test_valid_case(self):
+        ga = GrievanceAward
+        ga.recipient = self.person_pk
+        ga.case = self.case_pk
+        ga.awardAmount = 500.00
+        ga.description = ""
+        ga.date = '2016-12-01'
+        ga.full_clean()
+        ga.save()
+        self.assertTrue(True)
 
-# Test 3 - Validate event name, name 20 characters
-# Input: "01234567890126459789"
-# Expected result: Event created and added to DB
-    def test_event_name_valid_20_char(self):
-        testEvent = Event()
-        testEvent.name = "01234567890126459789"
-        testEvent.description = ""
-        testEvent.date = "2016-12-25"
-        testEvent.location = "Saskatoon"
-        testEvent.full_clean()
-        testEvent.save()
-
-# Test 4 - Validate event name, name 21 characters
-# Input: "012345678901234567890"
-# Expected result: Error thrown explaining event name limited to 20 characters
-    def test_event_name_21_chars_error(self):
+# Test 4 - Validate that an invalid case cannot be linked to a GrievanceAward
+# Input: case is an invalid case PK
+# Expected result: Error is thrown, record is not saved.
+    def test_valid_case(self):
         with self.assertRaises(ValidationError):
-            testEvent = Event()
-            testEvent.name = "012345678901264597890"
-            testEvent.description = ""
-            testEvent.date = "2016-12-25"
-            testEvent.location = "Saskatoon"
-            testEvent.full_clean()
-            testEvent.save()
+            ga = GrievanceAward
+            ga.recipient = self.person_pk
+            ga.case = -1
+            ga.awardAmount = 500.00
+            ga.description = ""
+            ga.date = '2016-12-01'
+            ga.full_clean()
+            ga.save()
+            self.assertTrue(True)
 
-# Test 5 - Validate event description, desc 50 characters
-# Input: "01234567890123456789012345678901234567890123456789"
-# Expected result: Event created and added to DB
-    def test_event_desc_50_chars(self):
-        testEvent = Event()
-        testEvent.name = "A"
-        testEvent.description = "01234567890123456789012345678901234567890123456789"
-        testEvent.date = "2016-12-25"
-        testEvent.location = "Saskatoon"
-        testEvent.full_clean()
-        testEvent.save()
-
-# Test 6 - Validate event description, desc 51 characters
-# Input: "012345678901234567890123456789012345678901234567890"
-# Expected result: Error thrown explaining event description is limited to 50 characters
-    def test_event_desc_51_chars_error(self):
+# Test 5 - Award Amount - Lower Bounds
+# Input: Award amount as 0.00
+# Expected result: Error is thrown, record is not saved
+    def test_amount_lower_invalid(self):
         with self.assertRaises(ValidationError):
-            testEvent = Event()
-            testEvent.name = "A"
-            testEvent.description = "012345678901234567890123456789012345678901234567890"
-            testEvent.date = "2016-12-25"
-            testEvent.location = "Saskatoon"
-            testEvent.full_clean()
-            testEvent.save()
+            ga = GrievanceAward
+            ga.recipient = self.person_pk
+            ga.awardAmount = 0.00
+            ga.description = ""
+            ga.date = '2016-12-01'
+            ga.full_clean()
+            ga.save()
 
-# Test 7 - Validate event description, empty desc
-# Input: ""
-# Expected result: Event created and added to DB
-    def test_event_desc_confirm_empty_desc(self):
-        testEvent = Event()
-        testEvent.name = "A"
-        testEvent.description = ""
-        testEvent.date = "2016-12-25"
-        testEvent.location = "Saskatoon"
-        testEvent.full_clean()
-        testEvent.save()
+# Test 6 - Award Amount - Valid
+# Input: Award amount as 100.00
+# Expected result: Record is saved to the DB.
+    def test_award_amount_valid(self):
+            ga = GrievanceAward
+            ga.recipient = self.person_pk
+            ga.awardAmount = 100.00
+            ga.description = ""
+            ga.date = '2016-12-01'
+            ga.full_clean()
+            ga.save()
+            self.assertTrue(True)
 
-# Test 8 - Validate event date, valid date
-# Input: "2016-11-17"
-# Expected result: Event created and added to DB
-    def test_event_valid_date(self):
-        testEvent = Event()
-        testEvent.name = "A"
-        testEvent.description = ""
-        testEvent.date = "2016-11-17"
-        testEvent.location = "Saskatoon"
-        testEvent.full_clean()
-        testEvent.save()
+# Test 7 - Award Amount - Upper bounds
+# Input: Award amount as 99999.99
+# Expected result: Record is saved to the DB.
+    def test_award_upper_valid(self):
+            ga = GrievanceAward
+            ga.recipient = self.person_pk
+            ga.awardAmount = 99999.99
+            ga.description = ""
+            ga.date = '2016-12-01'
+            ga.full_clean()
+            ga.save()
+            self.assertTrue(True)
 
-# Test 9 - Validate event date, invalid date
-# Input: "2016-11-35"
-# Expected result: Error thrown explaining a valid date must be entered
-    def test_event_invalid_date_format(self):
+# Test 8 - Award Amount - Upper Bounds Invalid
+# Input: Award amount as 100000.00
+# Expected result: Error is thrown, record is not saved
+    def test_amount_upper_invalid(self):
         with self.assertRaises(ValidationError):
-            testEvent = Event()
-            testEvent.name = "A"
-            testEvent.description = ""
-            testEvent.date = "2016-11-35"
-            testEvent.location = "Saskatoon"
-            testEvent.full_clean()
-            testEvent.save()
-
-# Test 10 - Validate event date, invalid date v2
-# Input: 'this is invalid'
-# Expected result: Error thrown explaining a valid date must be entered
-    def test_event_invalid_date_text(self):
-        with self.assertRaises(ValidationError):
-            testEvent = Event()
-            testEvent.name = "A"
-            testEvent.description = ""
-            testEvent.date = "this is invalid"
-            testEvent.location = "Saskatoon"
-            testEvent.full_clean()
-            testEvent.save()
-
-# Test 11 - Validate valid event location
-# Input "HONALULU"
-# Expected result: Event is created and added to DB
-    def test_event_location_valid(self):
-        testEvent = Event()
-        testEvent.name = "A"
-        testEvent.description = ""
-        testEvent.date = "2016-11-17"
-        testEvent.location = "HONALULU"
-        testEvent.full_clean()
-        testEvent.save()
-
-# Test 12 - Validate valid event location v2
-# Input: "REGINA"
-# Expected result: Event is created and added to DB
-    def test_event_location_valid2(self):
-        testEvent = Event()
-        testEvent.name = "A"
-        testEvent.description = ""
-        testEvent.date = "2016-11-17"
-        testEvent.location = "REGINA"
-        testEvent.full_clean()
-        testEvent.save()
-
-# Test 13: Validate an invalid event location
-# Input: "this city name is way too long"
-# Expected result: Error thrown explaining event location is too long
-    def test_event_location_too_long(self):
-        with self.assertRaises(ValidationError):
-            testEvent = Event()
-            testEvent.name = "A"
-            testEvent.description = ""
-            testEvent.date = "2016-11-17"
-            testEvent.location = "this city name is way too long"
-            testEvent.full_clean()
-            testEvent.save()
-
-# Test 14 - Validate an invalid event location v2
-# Input: ""
-# Expected result: Error thrown explaining event location cannot be left empty.
-    def test_event_location_empty(self):
-        with self.assertRaises(ValidationError):
-            testEvent = Event()
-            testEvent.name = "A"
-            testEvent.description = ""
-            testEvent.date = "2016-11-17"
-            testEvent.location = ""
-            testEvent.full_clean()
-            testEvent.save()
+            ga = GrievanceAward
+            ga.recipient = self.person_pk
+            ga.awardAmount = 100000.00
+            ga.description = ""
+            ga.date = '2016-12-01'
+            ga.full_clean()
+            ga.save()
