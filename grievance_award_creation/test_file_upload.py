65dd946e4b94d6028d010880679511be12aa878e
Daven Paul Casia <casia3115@saskpolytech.ca>
2017-01-12 09:30:00 -0600

    added comments in the unit tests -- Daven

--- grievance_award_creation/test_file_upload.py
+++ grievance_award_creation/test_file_upload.py
@@ -1,15 +1,39 @@
 from django.test import TestCase
+from .models import GrievanceAward, GrievanceFiles
+from django.core.files import File
+from add_case.models import Case
 
 class GrievanceFile_UploadTest(TestCase):
+    samplefile = File(name='Hello', file=None)
+    griev_f1 = GrievanceFiles()
+
+
+    #Create a grievance award
+    """
+    griev_aw = GrievanceAward(awardAmount=500,
+                              grievanceType='M',
+                              case=1,
+                              recipient=1,
+                              )
+      """
+
+
     def setUp(self):
-        pass
+        self.samplefile.size = 1000
+
 
     def test_user_can_upload_single_grievance_document(self):
         """
         Test if a user can associate a single document to a grievance ruling
         :return: None
         """
-        pass
+
+        """
+        self.griev_f1.file.file = self.samplefile
+        self.griev_f1.award = self.griev_aw
+        self.griev_f1.save()
+        """
+
 
     def test_user_can_upload_multiple_grievance_docs(self):
         """
@@ -20,14 +44,14 @@ class GrievanceFile_UploadTest(TestCase):
 
     def test_user_uploaded_document_is_in_the_correct_path_in_the_server(self):
         """
-        Test if user’s uploaded file exists in the server
+        Test if user's uploaded document exists in the server
         :return: None
         """
         pass
 
     def test_user_can_upload_if_the_total_file_size_is_less_than_500MB(self):
         """
-        Test if users’s uploaded file is less than 500MB
+        Test if user's uploaded file is less than 500Mb
         :return: None
         """
         pass
@@ -35,14 +59,14 @@ class GrievanceFile_UploadTest(TestCase):
 
     def test_user_cannot_upload_if_the_total_file_size_is_greater_than_500MB(self):
         """
-        Test if users’s uploaded file is less than 500MB
+        Test if users's uploaded file is less than 500MB
         :return: None
         """
         pass
 
     def test_user_can_only_upload_files_with_valid_extension(self):
         """
-        Test if user’s uploaded file only has the following extension:
+        Test if user's uploaded file only has the following extension:
         .docx, .pptx, .xlsx, .csv, .pdf, .txt and .msg
         :return: None
         """
