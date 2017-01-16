e4f9453e077155873347329e8585103e06a3e96b
Cam <auser5707@saskpolytech.ca>
2017-01-16 14:49:15 -0600

    Added a few new error messages for different invalid postal code entry cases. Also overrode the default max_length error message for postal code so that we could show a custom error instead.
    -- Cam
    -- David

--- add_member/test_edit_postal_code.py
+++ add_member/test_edit_postal_code.py
@@ -31,41 +31,59 @@ class PostalCodeTestCase(TestCase):
         self.tempPerson.full_clean()
         self.tempPerson.save()
 
+    # Test 1 - Validate a valid postal code
+    # Input: H0H 0H0
+    # Expected result: Postal code is saved as H0H 0H0
     def test_valid_postal_code(self):
         self.tempPerson.pCode = 'H0H 0H0'
         self.tempPerson.full_clean()
         self.tempPerson.save()
         self.assertTrue(self.tempPerson.pCode == 'H0H 0H0')
 
+    # Test 2 - Validate a valid lowercase postal code
+    # Input: h0h 0h0
+    # Expected result: Postal code is saved as H0H 0H0
     def test_lowercase_postal_code(self):
         self.tempPerson.pCode = 'h0h 0h0'
         self.tempPerson.full_clean()
         self.tempPerson.save()
         self.assertTrue(self.tempPerson.pCode == 'H0H 0H0')
 
+    # Test 3 - Validate a valid postal code without spaces
+    # Input: H0H 0H0
+    # Expected result: Postal code is saved as H0H 0H0
     def test_valid_postal_code_no_space(self):
         self.tempPerson.pCode = 'H0H0H0'
         self.tempPerson.full_clean()
         self.tempPerson.save()
         self.assertTrue(self.tempPerson.pCode == 'H0H 0H0')
 
+    # Test 4 - Validate an invalid postal code
+    # Input: H@H 0H0
+    # Expected result: Error is thrown
     def test_invalid_postal_code(self):
         #Change to specific error check
-        with self.assertRaises(ValidationError):
+        with self.assertRaisesRegexp(ValidationError, "Invalid postal code entered! Postal code must be in the form A#A #A#."):
             self.tempPerson.pCode = 'H@H 0H0'
             self.tempPerson.full_clean()
             self.tempPerson.save()
 
+    # Test 5 - Validate an invalid postal code (too long)
+    # Input: H0H 0H0H0
+    # Expected result: Error is thrown
     def test_invalid_postal_code_more_than_seven_characters(self):
         #Change to Specific error check
-        with self.assertRaises(ValidationError):
+        with self.assertRaisesRegexp(ValidationError, "Postal code entered is too long. Must be in the form A#A #A#."):
             self.tempPerson.pCode = 'H0H 0H0H0'
             self.tempPerson.full_clean()
             self.tempPerson.save()
 
+    # Test 6 - Validate an invalid postal code (too short)
+    # Input: H
+    # Expected result: Error is thrown
     def test_invalid_postal_code_one_character(self):
         # Change to Specific error check
-        with self.assertRaises(ValidationError):
+        with self.assertRaisesRegexp(ValidationError, "Postal code entered is too short. Must be in the form A#A #A#."):
             self.tempPerson.pCode = 'H'
             self.tempPerson.full_clean()
             self.tempPerson.save()
--- add_member/validators.py
+++ add_member/validators.py
@@ -3,9 +3,16 @@ import re
 
 #validators for postal code
 def validate_pCode(value):
-    PCODE_REGEX = re.compile(r"[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ][0-9][ABCEGHJKLMNPRSTVWXYZ][0-9]")
-    if not PCODE_REGEX.match(str(value).upper()):
-        raise ValidationError("Invalid postal code")
+    # Check to see if postal code is too short
+    if value:
+        if len(value) < 6:
+            raise ValidationError("Postal code entered is too short. Must be in the form A#A #A#.")
+        elif len(value) > 7:
+            raise ValidationError("Postal code entered is too long. Must be in the form A#A #A#.")
+        else:
+            PCODE_REGEX = re.compile(r"[a-zA-Z]\d[a-zA-Z]\s?\d[a-zA-Z]\d")
+            if not PCODE_REGEX.match(str(value)):
+                raise ValidationError("Invalid postal code entered! Postal code must be in the form A#A #A#.")
 
 #validators for phone numbers
 def validate_numbers(value):
