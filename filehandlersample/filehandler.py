633a7c244292a0dc65e4995f49bd6a2e6b6af1ca
Megan Kostiuk <kostiuk9589@saskpolytech.ca>
2017-01-18 13:52:01 -0500

    Did some changes, code was not working on server side --Megan and Daven

--- filehandlersample/filehandler.py
+++ filehandlersample/filehandler.py
@@ -1,39 +1,28 @@
 from django.core.files.uploadhandler import TemporaryFileUploadHandler
-from django.core.files.uploadhandler import StopUpload, SkipFile, FileUploadHandler
+from django.core.files.uploadhandler import StopUpload, SkipFile
 from spfa_mt.settings import MAX_FILE_SIZE
 from django.core.exceptions import ValidationError
 
 class ValidateUploadSize(TemporaryFileUploadHandler):
     def __init__(self, *args, **kwargs):
         super(ValidateUploadSize, self).__init__(*args, **kwargs)
-        self.total_size = 0
+        self.total_size = None
 
 
     def receive_data_chunk(self, raw_data, start):
         super(ValidateUploadSize, self).receive_data_chunk(raw_data, start)
-        #print(self.total_size)
-        self.total_size += len(raw_data)
         if self.total_size > MAX_FILE_SIZE:
-            #raise ValidationError('Upload size limit exceeded exception')
-            raise StopUpload(connection_reset=True)
-        return raw_data
+            raise ValidationError('Upload size limit exceeded exception')
 
 
-    # def handle_raw_input(self, input_data, META, content_length, boundary, encoding=None):
-    #     super(ValidateUploadSize, self).handle_raw_input(input_data, META, content_length, boundary, encoding=None)
-    #     self.total_size = content_length
 
-    def file_complete(self, file_size):
-        return None
+    def handle_raw_input(self, input_data, META, content_length, boundary, encoding=None):
+        super(ValidateUploadSize, self).handle_raw_input(input_data, META, content_length, boundary, encoding=None)
+        self.total_size = content_length
 
-class CustomUploadError(Exception):
-    pass
 
-# class CancelUpload(TemporaryFileUploadHandler):
-#     def receive_data_chunk(self, raw_data, start):
-#         super(CancelUpload, self).receive_data_chunk(raw_data, start)
-#         raise SkipFile
 
-class ErroringUploadHandler(FileUploadHandler):
+class CancelUpload(TemporaryFileUploadHandler):
     def receive_data_chunk(self, raw_data, start):
-        raise CustomUploadError("File exceeded limit of 500mb")
+        super(CancelUpload, self).receive_data_chunk(raw_data, start)
+        raise SkipFile
