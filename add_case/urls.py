4a35428e63e12dd0fb5c14fa26c182d610abb75c
Brett <larose2631@saskpolytech.ca>
2017-01-26 13:53:21 -0600

    Done precode, working urls, moved dictionaries to new file, moving cases app to add_case, etc. --Brett, Stephanie

--- add_case/urls.py
+++ add_case/urls.py
@@ -5,4 +5,7 @@ app_name = 'add_case'
 
 urlpatterns = [
     url(r'^$', views.CaseCreate.as_view(), name='case_add'),
+    url(r'^edit/(?P<pk>[-\w]+)$', views.UpdateCaseView.as_view(), name='case_edit'),
+    url(r'^list/$', views.CaseList.as_view(), name='case_list'),
+    url(r'^(?P<pk>[-\w]+)/$', views.CaseDetail.as_view(), name='case_detail'),
 ]
