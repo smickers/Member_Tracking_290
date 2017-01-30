2478ea0b58f743a45ec91ba9e5f66ccb8393de81
Jaryd <buck6651@saskpolytech.ca>
2017-01-30 14:41:42 -0600

    styled forms for contact log. removed the success page, user is now directed directly to the list page. -jaryd, cam

--- contact_log/urls.py
+++ contact_log/urls.py
@@ -1,14 +1,12 @@
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
 from . import views
 
 app_name = 'contact_log_creation'
 
 urlpatterns = [
-    # Setting up a base URL for creating a contact log
-    url(r'^$', views.ContactLogCreate.as_view(), name='contact_log_add'),
-    # A URL entry to redirect the user after submitting a contact log
-    url(r'^list/$', views.ContactLogList.as_view(), name='contact_log_list'),
+    url(r'^add/$', views.ContactLogCreate.as_view(), name='contact_log_add'),
+    url(r'^(list)/$', views.ContactLogList.as_view(), name='contact_log_list'),
     url(r'^edit/(?P<pk>[-\d]+)$', views.ContactLogEdit.as_view(), name='contact_log_edit'),
     url(r'(?P<pk>[-\d]+)', views.success, name='success'),
+    url(r'^.*$', views.ContactLogList.as_view(), name='contact_log_list'),
 ]
