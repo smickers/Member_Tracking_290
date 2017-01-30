6a8c2a64f2e4144cda34500a8524e02f3082f9f5
Jaryd <buck6651@saskpolytech.ca>
2017-01-30 14:52:26 -0600

    fixing unit tests -jaryd, cam

--- contact_log/tests.py
+++ contact_log/tests.py
@@ -1,16 +1,43 @@
 # SPFA MT CST Project
 # November 7, 2016
 from django.test import TestCase
+from add_member.models import Person
 from .models import contactLog
 # Create your tests here.
 class ContactLogTests(TestCase):
+    person1 = Person()
+
+    def setUp(self):
+        self.person1.memberID = 4204444
+        self.person1.firstName = 'First'
+        self.person1.middleName = 'Middle'
+        self.person1.lastName = 'Last'
+        self.person1.socNum = 123456789
+        self.person1.city = 'Sample City'
+        self.person1.mailAddress = 'Sample address'
+        self.person1.mailAddress2 = 'Sample Address 2'
+        self.person1.pCode = 's7k5j8'
+        self.person1.hPhone = '(306)812-1234'
+        self.person1.cPhone = '(306)812-1234'
+        self.person1.hEmail = 'sample@sample.com'
+        self.person1.campus = 'SASKATOON'
+        self.person1.jobType = 'FTO'
+        self.person1.committee = 'Sample Commitee'
+        self.person1.memberImage = 'image.img'
+        self.person1.bDay = '2012-03-03'
+        self.person1.hireDate = '2012-03-03'
+        self.person1.gender = 'MALE'
+        self.person1.membershipStatus = 'RESOURCE'
+        self.person1.programChoice = 'Sample Program'
+        self.person1.full_clean()
+        self.person1.save()
 
     # Test that when a valid date is entered, true is returned
     # Input: 2016-01-01
     # Expected return: true
     def test_validateDateOne(self):
         tempCLog = contactLog()
-        tempCLog.member = 1
+        tempCLog.member = self.person1
         tempCLog.description = 'Hello World'
         tempCLog.date = '2016-01-01'
         self.assertTrue(tempCLog.validateDate(tempCLog.date))
@@ -20,7 +47,7 @@ class ContactLogTests(TestCase):
     # Expected return: true
     def test_validateDateTwo(self):
         tempCLog = contactLog()
-        tempCLog.member = 1
+        tempCLog.member = self.person1
         tempCLog.description = 'Hello World'
         tempCLog.date = '2016-12-12'
         self.assertTrue(tempCLog.validateDate(tempCLog.date))
@@ -30,7 +57,7 @@ class ContactLogTests(TestCase):
     # Expected return: false
     def test_validateDateThree(self):
         tempCLog = contactLog()
-        tempCLog.member = 1
+        tempCLog.member = self.person1
         tempCLog.description = 'Hello World'
         tempCLog.date = 'January 1, 2016'
         self.assertFalse(tempCLog.validateDate(tempCLog.date))
@@ -40,7 +67,7 @@ class ContactLogTests(TestCase):
     # Expected return: false
     def test_validateDateFour(self):
         tempCLog = contactLog()
-        tempCLog.member = 1
+        tempCLog.member = self.person1
         tempCLog.description = 'Hello World'
         tempCLog.date = '30-02-2016'
         self.assertFalse(tempCLog.validateDate(tempCLog.date))
@@ -50,7 +77,7 @@ class ContactLogTests(TestCase):
     # Expected return: false
     def test_validateDateFive(self):
         tempCLog = contactLog()
-        tempCLog.member = 1
+        tempCLog.member = self.person1
         tempCLog.description = 'Hello World'
         tempCLog.date = '29-02-2015'
         self.assertFalse(tempCLog.validateDate(tempCLog.date))
@@ -60,7 +87,7 @@ class ContactLogTests(TestCase):
     # Expected return: true
     def test_validateDateSix(self):
         tempCLog = contactLog()
-        tempCLog.member = 1
+        tempCLog.member = self.person1
         tempCLog.description = 'Hello World'
         tempCLog.date = '2016-01-30'
         self.assertTrue(tempCLog.validateDate(tempCLog.date))
@@ -70,7 +97,7 @@ class ContactLogTests(TestCase):
     #Expected result: No error
     def test_validateDescriptionOne(self):
         tempCLog = contactLog()
-        tempCLog.member = 1
+        tempCLog.member = self.person1
         tempCLog.description = ''
         tempCLog.date = '2016-01-01'
         try:
@@ -84,7 +111,7 @@ class ContactLogTests(TestCase):
     # Expected result: No error
     def test_validateDescriptionTwo(self):
         tempCLog = contactLog()
-        tempCLog.member = 1
+        tempCLog.member = self.person1
         tempCLog.description = '01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678'
         tempCLog.date = '2016-01-01'
         try:
@@ -99,7 +126,7 @@ class ContactLogTests(TestCase):
     # Expected result: No error
     def test_validateDescriptionThree(self):
         tempCLog = contactLog()
-        tempCLog.member = 1
+        tempCLog.member = self.person1
         tempCLog.description = '012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789'
         tempCLog.date = '2016-01-01'
         try:
@@ -114,7 +141,7 @@ class ContactLogTests(TestCase):
     # Expected result: An error occurs
     def test_validateDescriptionFour(self):
         tempCLog = contactLog()
-        tempCLog.member = 1
+        tempCLog.member = self.person1
         tempCLog.description = 'A012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789'
         tempCLog.date = '2016-01-01'
         try:
@@ -141,22 +168,19 @@ class ContactLogTests(TestCase):
     # Input: 0
     # Expected result: All checks pass
     def test_validatememberIDTwo(self):
-        tempCLog = contactLog()
-        tempCLog.member = '0'
-        tempCLog.description = 'A'
-        tempCLog.date = '2016-01-01'
-        try:
+        with self.assertRaisesMessage(Person.DoesNotExist, "Person matching query does not exist."):
+            tempCLog = contactLog()
+            tempCLog.member = Person.objects.get(firstName='A ridiculous first name!')
+            tempCLog.description = 'A'
+            tempCLog.date = '2016-01-01'
             tempCLog.clean()
-            self.fail("Member ID 2 failed!")
-        except ValueError:
-            self.assertTrue(True)
 
     # Test that a valid member ID is accepted
     # Input: 1
     # Expected result: All checks pass
     def test_validatememberIDThree(self):
         tempCLog = contactLog()
-        tempCLog.member = 1
+        tempCLog.member = self.person1
         tempCLog.description = 'A'
         tempCLog.date = '2016-01-01'
         try:
@@ -170,27 +194,20 @@ class ContactLogTests(TestCase):
     # Input: 999999999
     # Expected result: All checks pass
     def test_validatememberIDFour(self):
-        tempCLog = contactLog()
-        tempCLog.member = 999999999
-        tempCLog.description = 'A'
-        tempCLog.date = '2016-01-01'
-        try:
+        with self.assertRaisesMessage(Person.DoesNotExist, "Person matching query does not exist."):
+            tempCLog = contactLog()
+            tempCLog.member = Person.objects.get(firstName='A ridiculous first name!')
+            tempCLog.description = 'A'
+            tempCLog.date = '2016-01-01'
             tempCLog.clean()
-            self.assertTrue(True)
-        except ValueError:
-            self.fail("Member ID 4 failed!")
-
 
     # Test that an invalid member ID is rejected
     # Input: 1000000000
     # Expected result: All checks pass
     def test_validatememberIDFive(self):
-        tempCLog = contactLog()
-        tempCLog.member = 1000000000
-        tempCLog.description = 'A'
-        tempCLog.date = '2016-01-01'
-        try:
+        with self.assertRaisesMessage(Person.DoesNotExist, "Person matching query does not exist."):
+            tempCLog = contactLog()
+            tempCLog.member = Person.objects.get(firstName='A ridiculous first name!')
+            tempCLog.description = 'A'
+            tempCLog.date = '2016-01-01'
             tempCLog.clean()
-            self.fail("Member ID 5 failed!")
-        except ValueError:
-            self.assertTrue(True)
