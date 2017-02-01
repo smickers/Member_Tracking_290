d50bc23a2334464e8e598986db57fbc44547337f
Stephanie Paige <mccallum0513@saskpolytech.ca>
2017-02-01 14:25:06 -0600

    Got my URLs working for create and details. Working on fixing up my unit testing ... need to create a validator for the integer values. Tried using clean() but it's too quick to break. --SM

--- award/models.py
+++ award/models.py
@@ -9,11 +9,20 @@ class EducationAward(models.Model):
     description = models.CharField(max_length=150, null=True)
     award_amount = models.IntegerField(max_length=5, null=True)
 
+    # TODO I need to make this into validators
+    def clean(self):
+        if len(str(self.award_amount)) > 5:
+            raise ValueError("Amount must be greater than $0 and less than $10,000.")
+        elif len(str(self.award_amount)) == 0:
+            raise ValueError("Award value is required.")
+        # elif not (int(self.award_amount) % 1 == 0):
+        #     raise ValueError()
+
     # Default get_absolute_url method
     def get_absolute_url(self):
-        return reverse(viewname='award:create_edu_award_detail/', kwargs={'pk': self.pk})
+        return reverse(viewname='award:edu_detail', kwargs={'pk': self.pk})
 
-    # Method: __str__ (toString)
+    # Method: __str__
     # Purpose: Return a string representation of an education award. Shows the description, and the award amount.
     def __str__(self):
-        return self.description + " ($" + self.award_amount + ")"
+        return self.description.__str__()
