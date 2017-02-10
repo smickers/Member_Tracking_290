3c9ea4ff3ebcc12dfde9bd94d4e274bf77161b1f
Cam <auser5707@saskpolytech.ca>
2017-02-10 11:50:51 -0600

    Committing my work so far. Made some progress on our validators (we had a logic error when validating awarded members and recipients) as well as on the visuals for the list of educational awards.
    -- Cam

--- award/models.py
+++ award/models.py
@@ -8,7 +8,7 @@ import validators
 from spfa_mt import kvp
 import datetime
 
- 
+
 # Models for Education Awards
 class EducationAward(models.Model):
 
@@ -36,13 +36,21 @@ class EducationAward(models.Model):
     def clean(self):
             # First, ensure both the member and recipient have been entered
             if (self.awardedMember != None and self.awardRecipient != None and self.awardRecipient != ""):
+
+            #if (self.awardedMember != None and (self.awardRecipient != None or self.awardRecipient != "")):
                 if EducationAward.objects.exclude(id=self.id).filter(awardedMember=self.awardedMember).filter(awardRecipient=self.awardRecipient).count() > 0:
                     raise ValidationError('Cannot assign recipient to more than one award')
             else:
-                if self.awardedMember != None and (self.awardRecipient == None or self.awardRecipient == ""):
+                # Is only member entered?
+                if self.awardedMember != None:# and (self.awardRecipient == None or self.awardRecipient == ""):
                     raise ValidationError('Cannot assign an award with only a member')
-                elif self.awardedMember == None and (self.awardRecipient != None or self.awardRecipient != ""):
-                    raise ValidationError('Cannot assign an award without an associcated member')
+                # Is only recipient entered?
+                #elif self.awardedMember == None and (self.awardRecipient != None or self.awardRecipient != ""):
+                # Either the recipient is None
+                # or the recipient is an empty string
+                elif self.awardRecipient != None and self.awardRecipient != "":
+                #elif self.awardRecipient != None:# or self.awardRecipient != "":
+                    raise ValidationError('Cannot assign an award without an associcated member. Recipient value: [' + self.awardRecipient.__str__() + ']')
 
 
 #Model for PD Award
