f218997b834a668141d281c5ba39ec08b33f8f84
bitnami <casia3115@saskpolytech.ca>
2017-02-13 22:10:51 +0000

    Added unit tests methods for s27. Some of the methods included relates to  testing of new modified fields of Case and Grievance Awards. --Daven (Overtime)

--- add_case/test_case_grievance_award_links.py
+++ add_case/test_case_grievance_award_links.py
@@ -1,7 +1,66 @@
 from django.test import TestCase
 
+
 class CaseLinksToGrievanceAwards(TestCase):
     """
     Purpose: Test the connection between grievance award its case
     """
-    pass
+
+    def setUp(self):
+        """
+        Setup method to initialize essential Objects
+        :return: None
+        """
+
+
+        pass
+
+
+    def test_grievance_award_can_be_associated_with_a_single_member_if_grievance_award_type_is_of_member(self):
+        """
+        Purpose: test to see if grievance award can only be associated with a single member if it is of type
+        member
+        :return:
+        """
+        pass
+
+    def test_grievance__award_cannot_be_associated_with_a_single_member_if_grievance_award_type_is_not_of_member(self):
+        """
+        Purpose: test to see if grievance award can only be associated with a single member if it is of type
+        member
+        :return:
+        """
+        pass
+
+    def test_grievance_award_can_be_associated_with_multiple_member_if_grievance_award_type_is_of_policy(self):
+        """
+        Purpose: test to see if grievance award can only be associated with a multiple member if it is of type
+        policy
+        :return:
+        """
+        pass
+
+    def test_grievance_award_cannot_be_associated_with_multiple_members_if_griev_aw_type_isnt_of_policy(self):
+        """
+        Purpose: test to see if grievance award can only be associated with a multiple member if it is of type
+        policy
+        :return:
+        """
+        pass
+
+    def test_gr_aw_recipient_is_equal_to_the_union_of_primary_complainant_and_additional_member_of_cases(self):
+        """
+        Assumption: The case in this test is of type policy
+        :return:
+        """
+        pass
+
+
+    def test_gr_aw_recipient_is_equal_to_the_primary_complainant_of_the_case(self):
+        """
+        Assumption: The case in this test is of type member
+        :return:
+        """
+        pass
+
+
