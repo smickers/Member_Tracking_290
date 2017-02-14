49c91dd5cd3d077f0a51bac773069b9ef0e15ae8
Stephanie Paige <mccallum0513@saskpolytech.ca>
2017-02-14 16:54:12 -0600

    Fixed some minor comments. Still good to demo. --SM --DL

--- spfa_mt/tests.py
+++ spfa_mt/tests.py
@@ -3,12 +3,13 @@ from bs4 import BeautifulSoup
 from django.test import Client
 
 
+# Test for navigation bar element in entire SPFA-MT application
 class TestAllSPFAMTElements(SimpleTestCase):
-    # Test that the navigation bar menu exists on all pages in the Case app
+    # Test that the navigation bar menu exists on all pages listed in urls.py
     def test_nav_bar_exists_on_all_case_pages(self):
         client = Client()
         source_code = client.get('spfa_mt.urls')
         soup = BeautifulSoup(source_code.content, "html.parser")
         navtag = soup.findAll("div", attrs={'for': True})
         for div in navtag:
-            self.assertTrue(div.has_attr("navbar"))
+            self.assertTrue(div.has_attr("main_navbar"))
