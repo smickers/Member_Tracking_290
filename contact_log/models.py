f6cb98b08e0c3de77e976c68e81fee7d1322fc28
Cam <auser5707@saskpolytech.ca>
2017-02-17 11:46:55 -0600

    I think I finally got the code working so that filenames can be shown in the details view of a contact log. Although this isn't part of our story, at least now I'm able to determine whether or not a contact log has an associated file, which will be used to determine whether or not a file upload input should be shown.
    -- Cam

--- contact_log/models.py
+++ contact_log/models.py
@@ -33,26 +33,30 @@ class contactLog(models.Model):
     # Function: __str__
     # Purpose: toString method for a contactLog object
     # Returns: A string representing the current object
-    if __name__ == '__main__':
-        def __str__(self):
-            return self.member.__str__() + " - " + self.description + " " + self.date.__str__()
+    #if __name__ == '__main__':
+    def __str__(self):
+        return self.member.__str__() + " - " + self.description + " " + self.date.__str__()
 
 
         # Function: containsFile
         # Purpose: Returns true or false, based on whether or not a file is associated with this
         # contact log
         # Returns: boolean
-        @property
-        def containsfile(self):
-            #return ContactLogFile.objects.filter(relatedContactLog=self.id).count() > 0
-            #return "Hello world"
-            return 5
+    @property
+    def containsfile(self):
+        #return ContactLogFile.objects.filter(relatedContactLog=self.id).count() > 0
+        #return "Hello world"
+        return 5
 
-        @property
-        def returnTrue(self):
-            print("\n\n\n\n\nreturning true!\n\n\n\n")
-            return True
+class contactLogFileCounter(models.Manager):
 
+    def get_files(self, instanceID):
+        print "Instance ID = " + str(instanceID)
+        print "Number of files"
+        print ContactLogFile.objects.get(relatedContactLog=contactLog.objects.get(id=instanceID))
+        #return ContactLogFile.objects.get(relatedConactLog=instance)
+        #return ContactLogFile.objects.get(relatedContactLog=instance)
+        return ContactLogFile.objects.get(relatedContactLog=contactLog.objects.get(id=instanceID)).fileName.name
 
 class ContactLogFile(models.Model):
 
@@ -78,4 +82,4 @@ class ContactLogFile(models.Model):
             raise ValidationError("Invalid File Extension")
 
     def __str__(self):
-        return str(self.file.name)
+        return str(self.fileName.name)
--- contact_log/views.py
+++ contact_log/views.py
@@ -2,8 +2,9 @@
 # November 7, 2016
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic import ListView
-from .models import contactLog, ContactLogFile
+from .models import contactLog, ContactLogFile, contactLogFileCounter
 from .forms import ContactLogForm
+from django.shortcuts import render
 
 # View ContactLogCreate
 # Purpose: Put together a form to allow the user to create a new
@@ -18,3 +19,23 @@ class ContactLogEdit(UpdateView):
 
 class ContactLogList(ListView):
     model = contactLog
+
+def contactLogDetail(request, pk):
+    cl = contactLog.objects.get(id=pk)
+    print "Contact Log: " + cl.__str__()
+    manager = contactLogFileCounter()
+    try:
+        #cl.file_name = "testing"
+        print("Reading file [0]!")
+        relatedFile = manager.get_files(cl.pk)
+        print("Reading file!")
+        #cl.file_name = file.fileName
+        #cl.file_name = "Some junk"
+        cl.file_name = relatedFile
+        # if the grievance award has no files associated, empty the fields and dont display the html
+    except:
+        cl.file_name = "hello world"
+        cl.file_desc = ""
+        #cl.file_date_uploaded = ""
+
+    return render(request, 'contact_log/contactlog_edit.html', {'object' : cl})
