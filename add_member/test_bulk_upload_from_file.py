6a82a552f20dff74d8db05ca3df8fc92108ec8d9
Jaryd <buck6651@saskpolytech.ca>
2017-03-14 13:53:20 -0600

    started unit tests. almost ready to precode --jaryd --daven

--- add_member/test_bulk_upload_from_file.py
+++ add_member/test_bulk_upload_from_file.py
@@ -1,28 +1,59 @@
 from django.test import TestCase, Client
 from django.db import IntegrityError
 from django.core.exceptions import ValidationError
-from add_member.models import Person
+from add_member.models import Person, MemberBulkTempData
+from rest_framework.test import APIRequestFactory
+from spfa_mt import settings
+from django.core.files import File
 
 # region Tests for Creating a person
 class PersonUploadTestCase(TestCase):
     """Test multiple users can be uploaded from an excel file"""
 
+    def __init__(self, *args, **kwargs):
+        super(PersonUploadTestCase, self).__init__(*args, **kwargs)
+        self.factory = APIRequestFactory()
+        self.CONST_FILE_PATH = settings.STATIC_ROOT + 'add_member/%s'
+        self.test_xlsx = self.CONST_FILE_PATH % 'member_upload.xlsx'
+        self.invalid_file = self.CONST_FILE_PATH % 'member_upload_invalid_file.jpeg'
+        self.path_largefile = self.CONST_FILE_PATH % 'dummylarge.xlsx'
+        self.path_emptyfile = self.CONST_FILE_PATH % 'empty.xlsx'
+
+
     def setUp(self):
         """Setup method to initialize essential Objects"""
+        f = open(self.path_largefile, "wb")
+        f.seek(settings.MAX_FILE_SIZE+1)
+        f.write("\0")
+        f.close()
+
+        f = open(self.path_emptyfile, "wb")
+        f.write("")
+        f.close()
 
     '''
     Name:       test_user_can_upload_excel_file
     Function:   Test user is able to upload an excel file (.xlsx)
     '''
     def test_user_can_upload_excel_file(self):
-        pass
+        fp = open(self.test_xlsx, "r")
+        xlsx_file = File(fp)
+
+        # TODO: Make an api endpoint that accepts a file and returns a JSON response.
+        request = self.factory.post('{{endpoint_url}}', {'file': xlsx_file}, format='json')
+        json_response = request.json()
+        self.assertTrue( json_response.count == 1 )
 
     '''
     Name:       test_user_cannot_upload_other_file_formats
     Function:   Test user is not able to upload any other file format
     '''
     def test_user_cannot_upload_other_file_formats(self):
-        pass
+        fp = open(self.invalid_file, "r")
+        invalid_file = File(fp)
+
+        request = self.factory.post('{{endpoint_url}}', {'file': invalid_file}, format='json')
+        self.assertTrue(request.status_code == 400)  # Bad request
 
     '''
     Name:       test_user_sees_preview_table_with_member_information
@@ -30,7 +61,18 @@ class PersonUploadTestCase(TestCase):
                 they upload an excel file
     '''
     def test_user_sees_preview_table_with_member_information(self):
-        pass
+        fp = open(self.test_xlsx, "r")
+        xlsx_file = File(fp)
+
+        request = self.factory.post('{{endpoint_url}}', {'file': xlsx_file}, format='json')
+        json_response = request.json()
+
+        #TODO: Create a view that accepts the file pk and return json repersentation of the excel file
+        request = self.factory.post('{{endpoint_url_of_xlsx_to_json_converter}}', {'id': json_response.id}, format='json')
+
+        json_response_of_converter = request.json()
+
+        self.assertTrue(json_response_of_converter != "")
 
     '''
     Name:       test_preview_table_displays_all_member_information
@@ -38,6 +80,9 @@ class PersonUploadTestCase(TestCase):
                 from the excel file
     '''
     def test_preview_table_displays_all_member_information(self):
+        """
+
+        """
         pass
 
     '''
@@ -45,14 +90,22 @@ class PersonUploadTestCase(TestCase):
     Function:   Test user cannot upload files that are greater than 500mb
     '''
     def test_user_cannot_upload_files_greater_than_500mb(self):
-        pass
+        fp = open(self.path_largefile, "r")
+        very_large = File(fp)
+
+        request = self.factory.post('{{endpoint_url}}', {'file': very_large}, format='json')
+        self.assertTrue(request.status_code == 400)  # Bad request
 
     '''
     Name:       test_preview_table_is_not_shown_if_excel_file_is_empty
     Function:   Test that member info preview table is not shown if uploaded excel file is empty
     '''
     def test_preview_table_is_not_shown_if_excel_file_is_empty(self):
-        pass
+        fp = open(self.path_emptyfile, "r")
+        empty_file = File(fp)
+
+        request = self.factory.post('{{endpoint_url}}', {'file': empty_file}, format='json')
+        self.assertTrue(request.status_code == 400)  # Bad request
 
     '''
     Name:       test_members_are_created_if_non_required_fields_are_missing_in_file
