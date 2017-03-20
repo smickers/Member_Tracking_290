4a30160965383fe82d9e57bfe618526197c2c8fe
David Letkeman <letkeman0812@saskpolytech.ca>
2017-03-20 14:51:22 -0600

    Merged in Master, Wrote the Meeting FIle Model David

--- meeting/models.py
+++ meeting/models.py
@@ -5,6 +5,8 @@ from add_member.models import Person
 from add_com.models import Committee
 from datetime import date
 from django.core.urlresolvers import reverse
+from spfa_mt import settings
+from .validators import *
 
 
 # Class: Meeting
@@ -25,4 +27,26 @@ class Meeting(models.Model):
 
 
 class MeetingFiles(models.Model):
-    """"""
+    # Attributes
+    dateUploaded = models.DateTimeField(auto_now=True, blank=True, null=True)
+    fileName = models.FileField(upload_to='meetings/', blank=True, null=True, validators=[validate_file_ext])
+    relatedMeeting = models.ForeignKey(Meeting, blank=True, null=True)
+    fileDesc = models.CharField(max_length=50, blank=True, null=True)
+
+    # Function: clean()
+    # Purpose: Override the existing clean method, in order to perform some file validation
+    def clean(self):
+        super(MeetingFiles, self).clean()
+        if self.fileName.size > settings.MAX_FILE_SIZE:
+            raise ValidationError('Upload size limit exceeded exception')
+        if self.fileName.size == 0:
+            raise ValidationError('The submitted file is empty.')
+        if self.fileName.name.split(".")[-1] not in settings.FILE_EXT_TO_ACCEPT:
+            # Check if the uploaded file has a valid extension
+            raise ValidationError("Invalid File Extension")
+
+    # Function: __str__()
+    # Purpose: Provide a readable naem for the file we uploaded
+    # Returns: filename, as a string.
+    def __str__(self):
+        return str(self.fileName.name)
--- meeting/validators.py
+++ meeting/validators.py
@@ -1,5 +1,7 @@
 from django.core.exceptions import ValidationError
-from add_member.models import Person
-from add_case.models import Case
-from meeting.models import Meeting
+from spfa_mt import settings
 
+# Validator for file upload extension
+def validate_file_ext(value):
+    if value.name.split(".")[-1] not in settings.FILE_EXT_TO_ACCEPT:
+        raise ValidationError('Invalid File Extension')
