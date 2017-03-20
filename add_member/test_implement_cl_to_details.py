6ae61f92fe96a117af25190246227d20659adb9d
Stephanie Paige <mccallum0513@saskpolytech.ca>
2017-03-20 13:15:01 -0600

    pulling in the new master before i get too far.--SM

--- add_member/test_implement_cl_to_details.py
+++ add_member/test_implement_cl_to_details.py
@@ -1,5 +1,14 @@
 from django.test import TestCase
+from .models import Person
+from .models import contactLog
 
 
+# Test to implement contact logs to a member's details page
 class TestImplementCLsToMemberDetails(TestCase):
-    """"""
+
+    def __init__(self, *args, **kwargs):
+        super(TestImplementCLsToMemberDetails, self).__init__(*args, **kwargs)
+        self.dw = Person()
+        self.kk = Person()
+        self.ww = Person()
+        self.cl_1 = contactLog
