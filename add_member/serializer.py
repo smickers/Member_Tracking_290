877857bebaa95c66f516c9173a2fe77ff863c749
Jaryd Buck <buck6651@saskpolytech.ca>
2017-03-21 19:02:48 +0000

    serializer.py edited online with Bitbucket

--- add_member/serializer.py
+++ add_member/serializer.py
@@ -1,7 +1,7 @@
 from drf_haystack.serializers import HaystackSerializer
 from add_member.search_indexes import MembersIndex
 from rest_framework import serializers
-from add_member.models import PersonFile
+from add_member.models import PersonFile, Person
 
 
 class MemberSearchSerializer(HaystackSerializer):
@@ -30,3 +30,14 @@ class MemberFileSerializer(serializers.ModelSerializer):
         instance = PersonFile(**attrs)
         instance.clean()
         return attrs
+        
+class MemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Person
+        fields = '__all__' 
+    
+    def validate(self, attrs): 
+        instance = Person(**attrs)
+        instance.clean()
+        return attrs
+    
