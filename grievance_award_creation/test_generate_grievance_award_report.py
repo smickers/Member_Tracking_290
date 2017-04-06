a9122a24a79bf4dc7dadb21b98d0dc8ffa34daa2
Jaryd <buck6651@saskpolytech.ca>
2017-04-06 11:41:11 -0600

    added unit tests file -- jb

---
+++ grievance_award_creation/test_generate_grievance_award_report.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+class GrievanceAwardReportTest(TestCase):
+
+    def test_if_user_can_download_a_pdf_version_of_the_report(self):
+        pass
+
+    def test_if_the_pdf_file_generated_is_a_valid_pdf(self):
+        pass
