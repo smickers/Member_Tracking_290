5d978e89b5bb9adb9cd673775f01e2690f9cc33c
Cam <auser5707@saskpolytech.ca>
2017-04-10 14:09:28 -0600

    Initial commit of some fixes that master needs.
    -- Daven

--- spfa_mt/views.py
+++ spfa_mt/views.py
@@ -1,5 +1,4 @@
 from django.views.generic import *
-from wkhtmltopdf.views import PDFTemplateView
 from add_member.models import Person
 
 # TODO: Move this view somewhere else. Maybe in the contact log app
@@ -8,13 +7,4 @@ class spfaView(TemplateView):
     template_name = 'index.html'
 
 
-class PDFView(PDFTemplateView):
-    template_name = 'test.html'
 
-    def get_context_data(self, **kwargs):
-        return super(PDFView, self).get_context_data(
-            pagesize="A4",
-            cls=Person.objects.all(),
-            title="Hi there!",
-            **kwargs
-        )
